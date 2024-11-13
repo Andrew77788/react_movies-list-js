ac5b8d48faf5f1265c60a9cda1d90d4e3f9c6bb6
Andrii Dotsulenko <58427426+Andrew77788@users.noreply.github.com>
2024-11-13 20:54:26 +0200

    add task solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,25 +1,10 @@
 import { MovieCard } from '../MovieCard/MovieCard';
-import moviesFromServer from '../../api/movies.json';
-
 import './MovieList.scss';
 
-// export const MovieList = () => (
-//   <div className="movies">
-//     {moviesFromServer.map(movie => (
-//       <MovieCard movie={movie} key={movie.imdbId} />
-//     ))}
-//   </div>
-// );
-
-// {movies.length === 0 ? <p>No movies available</p> : movies.map(movie => <MovieCard movie={movie} />)}
-
-function MovieList() {
-  if (moviesFromServer.length === 0) {
-    return (<p>No movies available</p>);
-  }
+function MovieList({movies}) {
   return (
     <div className="movies">
-      {moviesFromServer.map(movie => (
+      {movies.map(movie => (
         <MovieCard movie={movie} key={movie.imdbId} />
       ))}
     </div>
@@ -27,5 +12,3 @@ function MovieList() {
 }
 
 export { MovieList };
-
-
